1a7d8e31c37c4931d881733d7f49ed81a169efea
nufa_sempai <nufa_sempai@mail.ru>
2023-06-17 18:41:18 +0300

    ДЗ 21 - Stream API и optional - Выполнено - отредактировано

--- src/main/java/com/example/homework_21/exception/EmployeeStorageIsFullException.java
+++ src/main/java/com/example/homework_21/exception/EmployeeStorageIsFullException.java
@@ -1,5 +1,8 @@
 package com.example.homework_21.exception;
 
-public class EmployeeStorageIsFullException extends RuntimeException{
-}
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
+@ResponseStatus(code = HttpStatus.INTERNAL_SERVER_ERROR)
+public class EmployeeStorageIsFullException extends RuntimeException {
+}
